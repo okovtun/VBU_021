4c3ff85ac0330b9c1b951bda7f533fa610800a07
okovtun <okovtun@itstep.org>
2020-07-12 11:55:28 +0300

    Conspect.

---
+++ DataTypes/DataTypes/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	//cout << (bool)0.0000003 << endl;
+
+	/*char symbol = '@';
+	cout << symbol << endl;*/
+
+	cout << sizeof(short) << endl;
+	cout << SHRT_MIN << "..." << SHRT_MAX << endl;
+}
